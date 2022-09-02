d93b55e6200567acaf66c092170969dad942fb9b
hmanjun <harshith.hm03@gmail.com>
2022-09-01 18:53:57 -0700

    Add signToken util function

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 //Require different models here
 const Character = require('./character-model/Character')
+const User = require('./user-model/User')
 
 
-
-module.exports = {Character}
+module.exports = {Character, User}
