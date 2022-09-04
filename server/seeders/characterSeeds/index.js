317381a72caf8daa8766828fb712113d7d160376
hmanjun <harshith.hm03@gmail.com>
2022-09-03 19:31:49 -0700

    Add more seed data and limit how much is being seeded for testing

--- server/seeders/characterSeeds/index.js
+++ server/seeders/characterSeeds/index.js
@@ -1,3 +1,4 @@
+/* Add more characters later
 const yelanData = require('./yelan.json')
 const yaeData = require('./yae-miko.json')
 const ayatoData = require('./ayato.json')
@@ -5,7 +6,14 @@ const yunData = require('./yun-jin.json')
 const shenheData = require('./shenhe.json')
 const gorouData = require('./gorou.json')
 const ittoData = require('./arataki-itto.json')
+*/
+//Minimum needed characters
+const raidenData = require('./raiden-shogun.json')
+const xianglingData = require('./xiangling.json')
+const xingqiuData = require('./xingqiu.json')
+const bennetData = require('./bennet.json')
 
+/*
 module.exports = [
     ittoData,
     gorouData,
@@ -14,4 +22,12 @@ module.exports = [
     yaeData,
     ayatoData,
     yelanData
+]
+*/
+
+module.exports = [
+    raidenData,
+    xianglingData,
+    xingqiuData,
+    bennetData
 ]
