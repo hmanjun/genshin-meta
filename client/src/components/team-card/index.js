a9b4f65fa89706d2c693b59a706c9eea752bafa7
hmanjun <harshith.hm03@gmail.com>
2022-09-05 22:42:06 -0700

    Add team card component + styling

--- client/src/components/team-card/index.js
+++ client/src/components/team-card/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './style.css'
 
 const TeamCard = ({data}) => {
     return (
-        <div>
-            <h1>test</h1>
+        <div className='flex-col'>
+            <h2 style={{color: "white"}}>{data.name}</h2>
+            <div className='flex-wrap'>
+            {data.members.map((member) => (
+                <div className='talent-sumary-wrapper team-card-wrapper'>
+                    <img alt={`${member.name}`} src={require(`../../images/${member.imgPath}/icon.webp`)}/>
+                    <h4 className='talent-name team-name'>{member.name}</h4>
+                    <h5>{member.role}</h5>
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
