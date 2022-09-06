3a07b3ed50ad510cfb2b91c23a034fa0c2aed543
hmanjun <harshith.hm03@gmail.com>
2022-09-06 03:45:48 -0700

    Add heroku connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/genshin-meta',
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/genshinmeta',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
