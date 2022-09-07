d8065a01189675f16941be2fed78df2d6a8fda8c
hmanjun <harshith.hm03@gmail.com>
2022-09-07 10:17:20 -0700

    Add report delete button on comments

--- client/src/components/character-comments/index.js
+++ client/src/components/character-comments/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { useMutation } from '@apollo/client'
+import { REPORT_COMMENT } from '../../utils/mutations'
+import { DELETE_COMMENT } from '../../utils/mutations'
 import './style.css'
 
 const CommentContainer = ({comments}) => {
+    const [report, {errorR, dataRep}] = useMutation(REPORT_COMMENT)
+    const [deleteCom, {errorD, dataDel}] = useMutation(DELETE_COMMENT)
+
+    const reportComment = async(id) => {
+        try {
+            await report({
+                variables: {id: id}
+            })
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    const deleteComment = async(id) => {
+        try {
+            await deleteCom({
+                variables: {id: id}
+            })
+        } catch (err) {
+            console.error(e)
+        }
+    }
+
     return (
         <div>
             {comments.map((data) => (
@@ -11,6 +37,10 @@ const CommentContainer = ({comments}) => {
                         <h4 className='c-name'>{data.name}</h4>
                     </div>
                     <p style={{color: "white"}}>{data.body}</p>
+                    <button type='button' onClick={reportComment(data._id)}></button>
+                    {data.reports > 1 && (
+                        <button type="button" onClick={deleteCom}></button>
+                    )}
                 </div>
             ))}
         </div>
